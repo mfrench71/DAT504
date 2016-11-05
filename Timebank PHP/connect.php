81a6f01fe6cd4fefa76de6c9bca44328cddf4fca
mfrench71 <mfrench71@googlemail.com>
2016-11-05 15:04:21 +0000

    Delete connect.php

--- Timebank PHP/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once 'login.php';
-    $connection = new mysqli($db_hostname,$db_username,$db_password,$db_database);
-
-    // Check connection
-    if ($connection->connect_error) die ($connection->connect_error);
-?>
