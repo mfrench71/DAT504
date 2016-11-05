f3a4c5fce42bc276d7a6517ed069dccf9a721e8e
mfrench71 <mfrench71@googlemail.com>
2016-11-05 14:09:05 +0000

    Create meta.php

---
+++ Timebank PHP/meta.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="javascript.js"></script>
+        <link href="style.css" rel="stylesheet">
+        <title><?=$appname?></title>
+    </head>
+    <body>
