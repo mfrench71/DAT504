2c9387dfb94750f255b178a4ed2123a0126d6191
mfrench71 <mfrench71@googlemail.com>
2016-11-05 15:02:20 +0000

    Comments, etc.

--- Timebank PHP/checkuser.php
+++ Timebank PHP/checkuser.php
@@ -1,14 +1,28 @@
 <?php
-require_once 'functions.php';
-if (isset($_POST['username']))
-{
-$username = cleanString($_POST['username']);
-$result = queryMysql("SELECT * FROM users WHERE username='$username'");
-if ($result->num_rows)
-echo "&nbsp;&#x2718; " .
-"This username is taken";
-else
-echo "&nbsp;&#x2714; " .
-"This username is available";
-}
-?>
+
+    require_once 'functions.php';
+
+    // Has data been posted?
+    // If yes, clean string and query database for supplied username
+
+    if (isset($_POST['username'])) {
+        
+        $username = cleanString($_POST['username']);
+        $result = queryMysql("SELECT * FROM users WHERE username='$username'");
+        
+    }
+
+    // Is there a result?
+    // If yes, the username already exists
+
+    if ($result->num_rows) { ?>
+
+            <p>&nbsp;&#x2718; This username is taken</p>
+
+    <!-- If no, the username is available -->
+
+    <?php } else { ?>
+
+            <p>&nbsp;&#x2714; This username is available</p>
+    
+    <?php } ?>
