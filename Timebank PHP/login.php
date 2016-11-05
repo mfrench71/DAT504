e5e029a6a05026a0e8cb685cb1a916353bd277da
mfrench71 <mfrench71@googlemail.com>
2016-11-05 15:25:25 +0000

    Update login.php

--- Timebank PHP/login.php
+++ Timebank PHP/login.php
@@ -1,47 +1,61 @@
+<?php require_once 'header.php'; ?>
+
+<h3>Please enter your details to log in</h3>
+
 <?php
-require_once 'header.php';
-echo "<div class='main'><h3>Please enter your details to log in</h3>";
-$error = $username = $password = "";
-if (isset($_POST['username']))
-{
-$username = cleanString($_POST['username']);
-$password = cleanString($_POST['password']);
-if ($username == "" || $password == "")
-$error = "Not all fields were entered<br>";
-else
-{
-$result = queryMySQL("SELECT username ,password, creditBalance FROM users
-WHERE username='$username' AND password='$password'");
-$row = $result->fetch_assoc();
 
-if ($result->num_rows == 0)
-{
-    $error = "<span class='error'>Username/Password
-    invalid</span><br><br>";
-}
+    $error = $username = $password = "";
+
+    // Has the form been submitted?
+    // If yes, clean and set variables to whatever the user entered in the form
+
+    if (isset($_POST['username'])) {
+        $username = cleanString($_POST['username']);
+        $password = cleanString($_POST['password']);
+        
+        // Basic form validation
+        // If form fields incomplete, display error message
+
+        if ($username == "" || $password == "")
+            $error = "<p>Not all fields were completed.</p>";
+        
+        // If all form fields completed, query database for user details
+        
+        else {
+            $result = queryMySQL("SELECT username, password, creditBalance FROM users WHERE username='$username' AND password='$password'");
+            
+            // Fetch the results
+            
+            $row = $result->fetch_assoc();
+            
+            // If no results (no match), display error message
+
+            if ($result->num_rows == 0) {
+                $error = "<p>Username/Password invalid</p>";
+            }
+            
+            // If there is a result (match), set session variables and display logged in message
     
-else
+            else
     
-{
-    $_SESSION['username'] = $username;
-    $_SESSION['password'] = $password;
-    $_SESSION['creditBalance'] = $row['creditBalance'];
-    die("You are now logged in.<br><br>");
-}
+            {
+                $_SESSION['username'] = $username;
+                $_SESSION['password'] = $password;
+                $_SESSION['creditBalance'] = $row['creditBalance'];
+                die("You are now logged in.<br><br>");
+            }
     
-}
-}
-echo <<<_END
-<form method='post' action='login.php'>$error
-<span class='fieldname'>Username</span><input type='text'
-maxlength='255' name='username' value='$username'><br>
-<span class='fieldname'>Password</span><input type='password'
-maxlength='16' name='password' value='$password'>
-_END;
+        }
+    }
 ?>
-    <br> <span class='fieldname'>&nbsp;</span>
-    <input type='submit' value='Login'> </form>
-    <br> </div>
-    </body>
 
-    </html>
+<!-- Display Login Form -->
+
+<form method="post" action="login.php"><?=$error;?>
+<span class="fieldname">Username</span>
+    <input type="text" maxlength="255" name="username" value=<?=$username;?>>
+<br>
+<span class="fieldname">Password</span>
+    <input type="password" maxlength="16" name="password" value=<?=$password;?>>
+    <br><span class='fieldname'>&nbsp;</span>
+    <input type='submit' value='Login'></form>
