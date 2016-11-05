6240e43017cae158947f3c014bbd4e0536d0d9d3
mfrench71 <mfrench71@googlemail.com>
2016-11-05 14:18:02 +0000

    Removed embedded HTML

--- Timebank PHP/header.php
+++ Timebank PHP/header.php
@@ -1,10 +1,15 @@
 <?php
     session_start();
-    echo "<!DOCTYPE html>\n<html><head>";
     require_once 'functions.php';
+    require_once 'meta.php';
+
+    // Set default values
+
     $userstr = ' (Guest)';
     $creditBalanceStr = " (0)";
     
+    // If user is logged in, set username and credit balance from session
+
     if (isset($_SESSION['username'])) {
         $username = $_SESSION['username'];
         $creditBalance = $_SESSION['creditBalance'];
@@ -13,25 +18,26 @@
         $creditBalanceStr = " ($creditBalance)";
     
     }
-        
         else $loggedin = FALSE;
+    ?>
+    
+    <?=$appname . $userstr . " Credit Balance: " . $creditBalanceStr;?>
 
-        echo "<title>$appname$userstr</title>" .
-        $appname . $userstr . " Credit Balance: " . $creditBalanceStr;
-
-    if ($loggedin) {
+    <?php if ($loggedin) { ?>
         
-        echo "<br ><ul class='menu'>" .
-        "<li><a href='profile.php'>Edit Profile</a></li>" .
-        "<li><a href='logout.php'>Log out</a></li></ul><br>";
+        <ul class='menu'>
+            <li><a href='profile.php'>Edit Profile</a></li>
+            <li><a href='logout.php'>Log out</a></li>
+        </ul>
 
-    } else {
+    <?php } else { ?>
         
-        echo ("<br><ul class='menu'>" .
-        "<li><a href='index.php'>Home</a></li>" .
-        "<li><a href='signup.php'>Sign up</a></li>" .
-        "<li><a href='login.php'>Log in</a></li></ul><br>" .
-        "<span class='info'>&#8658; You must be logged in to " .
-        "view this page.</span><br><br>");
-    }
-?>
+        <ul class='menu'>
+            <li><a href='index.php'>Home</a></li>" .
+            <li><a href='signup.php'>Sign up</a></li>" .
+            <li><a href='login.php'>Log in</a></li>
+        </ul>
+
+        <span class='info'>&#8658; You must be logged in to view this page.</span>
+    
+    <?php } ?>
