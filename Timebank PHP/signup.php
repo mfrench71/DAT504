a7c27d920f16f983946d70748f7be840c31f6967
mfrench71 <mfrench71@googlemail.com>
2016-11-05 18:10:19 +0000

    Update signup.php

--- Timebank PHP/signup.php
+++ Timebank PHP/signup.php
@@ -1,29 +1,28 @@
-<?php
+<?php require_once 'header.php'; ?>
 
-    require_once 'header.php';
-
-    echo <<<_END
-    <script>
+<script>
+    
+    // If username field empty, clear info DIV
     function checkUser(username) {
         if (username.value == '') {
             O('info').innerHTML = ''
             return
         }
     
-    params = "username=" + username.value
-    request = new ajaxRequest()
-    request.open("POST", "checkuser.php", true)
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-    request.setRequestHeader("Content-length", params.length)
-    request.setRequestHeader("Connection", "close")
+        params = "username=" + username.value
+        request = new ajaxRequest()
+        request.open("POST", "checkuser.php", true)
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+        request.setRequestHeader("Content-length", params.length)
+        request.setRequestHeader("Connection", "close")
     
-    request.onreadystatechange = function() {
-        if (this.readyState == 4)
-            if (this.status == 200)
-                if (this.responseText != null)
-                    O('info').innerHTML = this.responseText
-    }
-    request.send(params)
+        request.onreadystatechange = function() {
+            if (this.readyState == 4)
+                if (this.status == 200)
+                    if (this.responseText != null)
+                        O('info').innerHTML = this.responseText
+        }
+        request.send(params)
     }
     
     function ajaxRequest() {
@@ -34,64 +33,57 @@
                 try { request = new ActiveXObject("Microsoft.XMLHTTP") }
                 catch(e3) {
                     request = false
-                        } 
-                    } 
-                }
+                } 
+            } 
+        }
                 
-                return request
+        return request
     }
-    </script>
-
-<div class='main'><h3>Please enter your details to sign up</h3>
-_END;
-
-$error = $username = $password = $firstname = $lastname = "";
-if (isset($_SESSION['username'])) destroySession();
-if (isset($_POST['username']))
-{
-$username = cleanString($_POST['username']);
-$password = cleanString($_POST['password']);    
-$firstname = cleanString($_POST['firstname']);    
-$lastname = cleanString($_POST['lastname']);    
-if ($username == "" || $password == "" || $firstname == "" || $lastname == "")
-$error = "Not all fields were completed<br><br>";
-else
-{
-$result = queryMysql("SELECT * FROM users WHERE username='$username'");
-if ($result->num_rows)
-$error = "That username already exists<br><br>";
-else
-{
-queryMysql("INSERT INTO users
-(username, password, firstname, lastname, timeBalance, creditBalance)
-VALUES('$username', '$password', '$firstname', '$lastname','0','1')");
-die("<h4>Account created</h4>Please Log in.<br><br>");
-}
-}
-}
-echo <<<_END
-<form method='post' action='signup.php'>$error
-
-<span class='fieldname'>Username</span>
-<input type='text' maxlength='255' name='username' value='$username'
-onBlur='checkUser(this)'><span id='info'></span><br>
     
-<span class='fieldname'>Password</span>
-<input type='text' maxlength='16' name='password'
-value='$password'><br>
+</script>
 
-<span class='fieldname'>First Name</span>
-<input type='text' maxlength='255' name='firstname'
-value='$firstname'><br>
+<h3>Please enter your details to sign up</h3>
 
-<span class='fieldname'>Last Name</span>
-<input type='text' maxlength='255' name='lastname'
-value='$lastname'><br>
+<?php
 
-_END;
+    $error = $username = $password = $firstname = $lastname = "";
+    if (isset($_SESSION['username'])) destroySession();
+
+    if (isset($_POST['username'])) {
+        $username = cleanString($_POST['username']);
+        $password = cleanString($_POST['password']);    
+        $firstname = cleanString($_POST['firstname']);    
+        $lastname = cleanString($_POST['lastname']);    
+
+        if ($username == "" || $password == "" || $firstname == "" || $lastname == "")
+            $error = "Not all fields were completed<br><br>";
+        else {
+            $result = queryMysql("SELECT * FROM users WHERE username='$username'");
+            if ($result->num_rows)
+                $error = "That username already exists<br><br>";
+            else {
+                queryMysql("INSERT INTO users (username, password, firstname, lastname, timeBalance, creditBalance) VALUES('$username', '$password', '$firstname', '$lastname','0','1')");
+                die("<h4>Account created</h4>Please Log in.<br><br>");
+            }
+        }
+    }
 ?>
-<span class='fieldname'>&nbsp;</span>
-<input type='submit' value='Sign up'>
-</form></div><br>
-</body>
-</html>
+
+<form method='post' action='signup.php'><?=$error;?>
+
+    <span class='fieldname'>Username</span>
+    <input type='text' maxlength='255' name='username' onBlur="checkUser(this)" value=<?=$username;?>><span id='info'></span><br>
+
+    <span class='fieldname'>Password</span>
+    <input type='text' maxlength='16' name='password' value=<?=$password;?>><br>
+
+    <span class='fieldname'>First Name</span>
+    <input type='text' maxlength='255' name='firstname' value=<?=$firstname;?>><br>
+
+    <span class='fieldname'>Last Name</span>
+    <input type='text' maxlength='255' name='lastname' value=<?=$lastname;?>><br>
+
+    <span class='fieldname'>&nbsp;</span>
+    <input type='submit' value='Sign up'>
+
+</form>
